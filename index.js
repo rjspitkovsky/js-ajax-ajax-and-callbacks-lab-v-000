26b74f024d304b5a0c764356b744cd56c945a5e2
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-12 23:03:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function searchRepositories() {
       <a href=${item.html_url}>View on GitHub</a><br>
       <p>Owner: ${item.owner.login} - <a href="${item.owner.html_url}">View Profile</a></p>
       <img src=${item.owner.avatar_url}><br>
+
+
       <!--<a href="#" onclick="showCommits(this)" data-commits-url="${item.url}/commits">Show Commits</a><br>-->
+
+
       <a href="#" onclick="showCommits(this)" data-repository=${item.name} data-owner=${item.owner.name}>Show Commits</a><br>
     </li>`).join("") + "</ul>"
 
